608e165a760d950c9eb9a3a50bba43c937747548
jayraj <jayraj2106@gmail.com>
2026-06-24 23:14:05 +0530

    add some html file

--- Vendsys/products/views.py
+++ Vendsys/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+def product_detail(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'products/product_detail.html', {'product': product})
+
+
