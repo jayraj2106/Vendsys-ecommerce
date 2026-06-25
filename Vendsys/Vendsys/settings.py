a152a8237d66392991c4d7836b3d69998f870316
jayraj <jayraj2106@gmail.com>
2026-06-25 17:20:55 +0530

    feat: setup global and app static/templates with base layout and css integration

--- Vendsys/Vendsys/settings.py
+++ Vendsys/Vendsys/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Vendsys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates_global'],  # global templates folder
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static_global",   # global static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
