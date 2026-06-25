3ff42e699c37f03941e626df073f85f2bded122d
jayraj <jayraj2106@gmail.com>
2026-06-25 21:00:36 +0530

    create forms.py create model and register in views.py

--- Vendsys/accounts/views.py
+++ Vendsys/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from Vendsys.accounts.forms import RegisterForm
 
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to the login page after successful registration
+    else:
+        form = RegisterForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
 # Create your views here.
