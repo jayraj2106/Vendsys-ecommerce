1fe0eca6c02e2871be91d79f1e337b06730785a5
jayraj <jayraj2106@gmail.com>
2026-07-15 00:11:19 +0530

    feat: add product filtering and search functionality

--- Vendsys/api/serializers/product_serializers.py
+++ Vendsys/api/serializers/product_serializers.py
@@ -6,3 +6,7 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
         
+class ProductFilterSerializer(serializers.Serializer):
+    category = serializers.IntegerField(required=False)
+    min_price = serializers.FloatField(required=False)
+    max_price = serializers.FloatField(required=False)
--- Vendsys/api/views/product_api.py
+++ Vendsys/api/views/product_api.py
@@ -1,10 +1,35 @@
 from products.models import Product
-from api.serializers.product_serializers import ProductSerializer
+from api.serializers.product_serializers import ProductSerializer,ProductFilterSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 
 
-class ProductViewSet(ModelViewSet): 
-    queryset = Product.objects.all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()     
     serializer_class = ProductSerializer
-    http_method_names = ['get'] #can add post for upload a product
+    http_method_names = ['get'] 
+    #can add post for upload a product
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'description']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # validate query params
+        filter_serializer = ProductFilterSerializer(data=self.request.GET)
+        filter_serializer.is_valid(raise_exception=True)
+        data = filter_serializer.validated_data
+
+        # apply filters
+        if "category" in data:
+            queryset = queryset.filter(category_id=data["category"])
+
+        if "min_price" in data:
+            queryset = queryset.filter(price__gte=data["min_price"])
+
+        if "max_price" in data:
+            queryset = queryset.filter(price__lte=data["max_price"])
+
+        return queryset
 
