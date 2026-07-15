8cc7339b97847762f765afcd35d72696b295f61c
jayraj <jayraj2106@gmail.com>
2026-07-15 23:49:48 +0530

    feat: add pagination to product API

--- Vendsys/api/serializers/product_serializers.py
+++ Vendsys/api/serializers/product_serializers.py
@@ -10,3 +10,7 @@ class ProductFilterSerializer(serializers.Serializer):
     category = serializers.IntegerField(required=False)
     min_price = serializers.FloatField(required=False)
     max_price = serializers.FloatField(required=False)
+
+    #pagination
+    page = serializers.IntegerField(required=False, min_value=1, default=1)
+    page_size = serializers.IntegerField(required=False, min_value=1, max_value=50, default=10)
--- Vendsys/api/views/product_api.py
+++ Vendsys/api/views/product_api.py
@@ -2,6 +2,7 @@ from products.models import Product
 from api.serializers.product_serializers import ProductSerializer,ProductFilterSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import filters
+from rest_framework.response import Response
 
 
 class ProductViewSet(ModelViewSet):
@@ -10,18 +11,18 @@ class ProductViewSet(ModelViewSet):
     http_method_names = ['get'] 
     #can add post for upload a product
 
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter,filters.OrderingFilter]
     search_fields = ['name', 'description']
+    ordering_fields = ['price']
+
 
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        # validate query params
         filter_serializer = ProductFilterSerializer(data=self.request.GET)
         filter_serializer.is_valid(raise_exception=True)
         data = filter_serializer.validated_data
 
-        # apply filters
         if "category" in data:
             queryset = queryset.filter(category_id=data["category"])
 
@@ -32,4 +33,32 @@ class ProductViewSet(ModelViewSet):
             queryset = queryset.filter(price__lte=data["max_price"])
 
         return queryset
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
 
+        queryset = self.filter_queryset(queryset)
+
+        filter_serializer = ProductFilterSerializer(data=request.GET)
+        filter_serializer.is_valid(raise_exception=True)
+        data = filter_serializer.validated_data
+
+        page = data.get("page", 1)
+        page_size = data.get("page_size", 10)
+
+        total_items = queryset.count()
+
+        start = (page - 1) * page_size
+        end = start + page_size
+
+        queryset = queryset[start:end]
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response({
+            "page": page,
+            "page_size": page_size,
+            "total_items": total_items,
+            "total_pages": (total_items + page_size - 1) // page_size,
+            "results": serializer.data
+        })
