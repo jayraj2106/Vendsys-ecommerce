34f6fae4e213f5a0eaa504c3521d45f29949bf03
jayraj <jayraj2106@gmail.com>
2026-07-20 00:51:48 +0530

    feat: add Stripe webhook to update order after successful payment

--- Vendsys/api/views/payment_api.py
+++ Vendsys/api/views/payment_api.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 from orders.models import Order
 from payments.models import Payment
 
@@ -12,6 +13,8 @@ from datetime import timedelta
 
 import stripe
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -60,7 +63,7 @@ class CreatePaymentAPIview(APIView):
 
         # create stripe checkout session
         checkout_session = stripe.checkout.Session.create(
-            payment_method_types=["card"],
+            payment_method_types=['card'],
             mode="payment",
             line_items=[
                 {
@@ -74,18 +77,18 @@ class CreatePaymentAPIview(APIView):
                     "quantity": 1,
                 }
             ],
+            metadata={
+                "order_id": str(order.id),
+            },
             success_url="http://127.0.0.1:8000/api/payments/payment-success/",
             cancel_url="http://127.0.0.1:8000/api/payments/payment-cancle/",
         )
 
-
-        # save stripe data in payment
+    
         payment.stripe_session_id = checkout_session.id
-        payment.stripe_payment_intent = checkout_session.payment_intent
         payment.save()
 
 
-        # return response
         return Response(
             {
                 "checkout_url": checkout_session.url
@@ -103,8 +106,66 @@ def payment_success(request):
     })
 
 @api_view(["GET"])
-def payment_cancle(request):
+def payment_cancel(request):
     return Response({
         "status": "cancle",
         "message": "Payment incompleted",
     })
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META.get("HTTP_STRIPE_SIGNATURE")
+    endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
+    
+    try:
+        event = stripe.Webhook.construct_event(
+            payload,
+            sig_header,
+            endpoint_secret
+        )
+
+        event_type = event["type"]
+        
+        print("WEBHOOK HIT:", event_type)
+
+        if event_type == "checkout.session.completed":
+            session = event["data"]["object"]
+
+            stripe_session_id = session["id"]
+            payment_status = session["payment_status"]
+
+            payment_intent = session["payment_intent"]
+
+            try:
+                payment = Payment.objects.get(stripe_session_id=stripe_session_id)
+
+            except Payment.DoesNotExist:
+                print("Payment not found")
+                return JsonResponse({"error": "Payment not found"}, status=404)
+
+            
+            if payment_status == "paid":
+                payment = Payment.objects.get(stripe_session_id=stripe_session_id)
+                
+                order_id = session.metadata.get("order_id")
+                order = Order.objects.get(id=order_id)
+
+                payment.stripe_payment_intent = payment_intent
+                payment.status = "SUCCESS"
+                payment.save()
+
+                # update related order
+                order = payment.order
+                order.status = "paid"
+                order.save()
+
+        print("Payment and Order updated successfully")
+
+    except ValueError:
+        return JsonResponse({"error": "Invalid payload"}, status=400)
+    
+    except stripe.error.SignatureVerificationError:
+        return JsonResponse({"error": "Invalid signature"}, status=400)
+
+    return JsonResponse({"status": "success"})
--- Vendsys/payments/urls.py
+++ Vendsys/payments/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from api.views.payment_api import CreatePaymentAPIview,payment_success,payment_cancle
+from api.views.payment_api import (CreatePaymentAPIview,
+                                   payment_success,
+                                   payment_cancel,
+                                   stripe_webhook)
 
 urlpatterns = [
     path("create/", CreatePaymentAPIview.as_view(), name="create-payment"),
     path("payment-success/", payment_success, name="payment-success"),
-    path("payment-cancle/", payment_cancle, name="payment-cancle"),
+    path("payment-cancel/", payment_cancel, name="payment-cancel"),
+    path("webhook/", stripe_webhook, name="stripe-webhook"),
 ]
