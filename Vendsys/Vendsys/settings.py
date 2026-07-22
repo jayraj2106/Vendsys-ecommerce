d2ea0af192578a7b0838865b8eb19978a1f1df2a
jayraj <jayraj2106@gmail.com>
2026-07-22 18:21:32 +0530

    fix: image issue

--- Vendsys/Vendsys/settings.py
+++ Vendsys/Vendsys/settings.py
@@ -36,8 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG") == "True"
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
